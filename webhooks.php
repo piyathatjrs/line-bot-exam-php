7ef70a3b567ea4d782205a9d9a54614381f487ba
piyathatjrs <43405957+piyathatjrs@users.noreply.github.com>
2020-02-06 20:11:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'EOvPQNqZOdFEUngJUHrJsGiYzmuoLYUXxVcsV3r7g4O9XqYAoeJvEjV20KMK0oteRqLwRNwQF6uI8wFA5+Tq3M9ut/VMr0y6UwhPmMTEj3KWBZdEtIydmjFI+gshmo1BiEmIZufPH1mGzETqFS0gQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
